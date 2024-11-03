bbd1a0e60a144c1a0990dafecbbb0de5be5b392c
mendezchacinga <gustavo2004mendez@gmail.com>
2024-11-02 23:23:55 -0400

    Funciones y logica del componente Home

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -5,7 +5,57 @@ import style from './Home.module.css';
 
 function Home({ tema }) {
   const [peliculasDelMomento, setPeliculasDelMomento] = useState([]);
-  
+  const [loading, setLoading] = useState(false);
+  const [queryMovie, setQueryMovie] = useState('');
+
+  const getPeliculasPopulares = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(URL_PELICULAS_POPULARES);
+      const data = await response.json();
+      setPeliculasDelMomento(data.results);
+      localStorage.setItem('movies', JSON.stringify(data.results));
+    } catch (error) {
+      console.error('Error al obtener películas populares:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getQueryMovie = useCallback(async (query) => {
+    if (query.length < 3) return;
+    try {
+      setLoading(true);
+      const response = await fetch(URL_QUERY_PELICULA(query));
+      const data = await response.json();
+      if (data.results.length) {
+        const top_12_peliculas = data.results.slice(0, 12);
+        setPeliculasDelMomento(top_12_peliculas);
+      }
+    } catch (error) {
+      console.error('Error al buscar películas:', error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  // Implementación de debounce
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      if (queryMovie) {
+        getQueryMovie(queryMovie);
+      } else {
+        const firstMovies = JSON.parse(localStorage.getItem('movies') || '[]');
+        setPeliculasDelMomento(firstMovies);
+      }
+    }, 300); // 300ms de retraso
+
+    return () => clearTimeout(debounceTimer);
+  }, [queryMovie, getQueryMovie]);
+
+  useEffect(() => {
+    getPeliculasPopulares();
+  }, []);
 
   return (
     <main className={`${style.contenedor} ${style[tema]}`}>
