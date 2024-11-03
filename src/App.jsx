99941472998e299721bccb7aaccee7fd70920ce6
mendezchacinga <gustavo2004mendez@gmail.com>
2024-11-02 23:11:13 -0400

    Dark mode y app router configuracion en el archivo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Home from "./paginas/Home";
+import Detalles from "./paginas/Detalles";
+import style from './App.module.css';
 
-export default function App() {
+function App() {
+  const [tema, setTema] = useState('claro');
+  
+  useEffect(() => {
+    // Verificar la preferencia del usuario al cargar la aplicación
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setTema(prefersDarkMode ? 'oscuro' : 'claro');
+
+  }, []);
+
+  const toggleTema = () => {
+    setTema(prevTema => prevTema === 'claro' ? 'oscuro' : 'claro');
+  };
+  
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div className={`${style.mainStyle} ${style[tema]}`}>
+        <button 
+          onClick={toggleTema} 
+          className={`${style.temaToggle} ${style[tema]}`}
+          aria-label={`Cambiar a modo ${tema === 'claro' ? 'oscuro' : 'claro'}`}
+        >
+          {tema === 'claro' ? '🌙' : '☀️'}
+        </button>
+        <Routes>
+          <Route path="/" element={<Home tema={tema} />} />  
+          <Route path="/detalles" element={<Detalles tema={tema} />} />          
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
